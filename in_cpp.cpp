c3d33f60f7c82a3285bda72463fc71ec3c05db5b
Berat Tezer <tezerberat42@gmail.com>
2023-12-06 14:55:03 +0300

    Upload 6.12.23

--- in_cpp.cpp
+++ in_cpp.cpp
@@ -2,8 +2,5 @@
 using namespace std;
 
 int main() {
-    char name;
-    cin >> name;
-    cout << name;
     return 0;
 }
--- out_cpp.cpp
+++ out_cpp.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main() {
-    cout << "world";
+    cout << "Hello World?";
     return 0;
 }
